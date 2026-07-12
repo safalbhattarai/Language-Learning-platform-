142ca6b70f0760d3fc95f217e610aad463c58d1d
safalbhattarai <safalbhattarai1416@gmail.com>
2026-07-12 15:30:35 +0545

    Navbar.jsx

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>LinguaLearn</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>Courses</li>
+        <li>Login</li>
+        <li>Register</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
