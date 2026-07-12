c815140ad1879555c24e36a74f342a62904f43c3
safalbhattarai <safalbhattarai1416@gmail.com>
2026-07-12 15:31:44 +0545

    Hero.jsx

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+function Hero() {
+  return (
+    <div>
+      <h1>Learn Languages Online</h1>
+
+      <p>
+        Master English, Japanese, Korean, Spanish and more.
+      </p>
+
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default Hero;
