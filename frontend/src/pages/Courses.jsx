8fcd085516f505dcbece5c41f1555ba7c001f08e
safalbhattarai <safalbhattarai1416@gmail.com>
2026-07-12 15:45:39 +0545

    course.jsx adding code

--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -1,5 +1,24 @@
 function Courses() {
-  return <h1>Courses Page</h1>;
+  return (
+    <div>
+      <h1>Available Courses</h1>
+
+      <div>
+        <h3>English Course</h3>
+        <p>Beginner to Advanced</p>
+      </div>
+
+      <div>
+        <h3>Japanese Course</h3>
+        <p>N5 to N1</p>
+      </div>
+
+      <div>
+        <h3>Korean Course</h3>
+        <p>TOPIK Preparation</p>
+      </div>
+    </div>
+  );
 }
 
 export default Courses;
