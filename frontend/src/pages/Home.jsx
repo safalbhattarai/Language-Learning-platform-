4275acbeb6dd024fd925365a927e430e01cb527b
safalbhattarai <safalbhattarai1416@gmail.com>
2026-07-13 12:56:43 +0545

    Dashboard.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,6 +8,7 @@ import CoursesPreview from "../components/CoursesPreview";
 import TrustedBy from "../components/TrustedBy";
 import CallToAction from "../components/CallToAction";
 
+
 function Home() {
   return (
     
