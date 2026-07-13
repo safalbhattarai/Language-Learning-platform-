1cd6a434141bc09f18f2496da8f0898216a5d839
safalbhattarai <safalbhattarai1416@gmail.com>
2026-07-13 10:40:04 +0545

    pushing

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+
+const authRoutes = require("./routes/authRoutes");
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -8,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
@@ -26,3 +29,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server Running on Port ${PORT}`);
 });
+
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,18 +1,64 @@
+import { useState } from "react";
+import API from "../services/api";
+import "./Register.css";
+
 function Register() {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await API.post("/auth/register", form);
+      alert(res.data.message);
+    } catch (err) {
+      alert("Registration Failed");
+    }
+  };
+
   return (
-    <div>
-      <h1>Create Account</h1>
+    <div className="register-container">
+      <div className="overlay">
+        <form className="register-form" onSubmit={handleSubmit}>
+          <h1>Learn Languages Faster</h1>
 
-      <input type="text" placeholder="Name" />
-      <br /><br />
+          <input
+            type="text"
+            name="name"
+            placeholder="Full Name"
+            onChange={handleChange}
+          />
 
-      <input type="email" placeholder="Email" />
-      <br /><br />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={handleChange}
+          />
 
-      <input type="password" placeholder="Password" />
-      <br /><br />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={handleChange}
+          />
 
-      <button>Register</button>
+          <button type="submit">
+            Create Account
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
